4e5d4232f44761de801f016059aba59b43063a21
tomasell <alex.tomasello@gmail.com>
2013-10-28 10:59:52 +0100

    change model

--- src/Bread/Media/File/Model.php
+++ src/Bread/Media/File/Model.php
@@ -1,13 +1,69 @@
 <?php
-
 namespace Bread\Media\File;
 
-class Model
+use Bread\Configuration\Manager as Configuration;
+use Bread\DAV\Properties;
+use Bread\DAV;
+use Bread\REST\Routing\URI\Template;
+
+
+class Model extends DAV\Model
 {
-    use Bread\DAV\Properties;
-    
-    public $name;
-    public $type;
-    public $data;
-    public $size;
+
+    protected $id;
+
+    protected $name;
+
+    protected $data;
+
+    protected $size;
+
+    protected $type;
+
+    public function href($variable = array())
+    {
+        return $variable[0] . $this->name;
+    }
+
+    public function getLiveProperties()
+    {
+        return array(
+            'getcontentlanguage' => 'it-IT',
+            'getcontentlength' => isset($this->size) ? $this->size : 0,
+            'getcontenttype' => isset($this->type) ? $this->type : null,
+            'getetag' => isset($this->name) ? md5($this->name) : uniqid(),
+            'resourcetype' => null,
+            'supportedlock' => 'default'
+        );
+    }
 }
+
+Configuration::defaults('Bread\Media\File\Model', array(
+    'keys' => array(
+        'id'
+    ),
+    'properties' => array(
+        'id' => array(
+            'type' => 'integer',
+            'strategy' => 'autoincrement'
+        ),
+        'data' => array(
+            'type' => 'this'
+        ),
+        'name' => array(
+            'type' => 'string'
+        ),
+        'size' => array(
+            'type' => 'string'
+        ),
+        'type' => array(
+            'type' => 'string'
+        )
+    ),
+    'storage' => array(
+        'options' => array(
+            'table' => 'files'
+        )
+    ),
+    'uri_template' => '/files{/name}{.accept}'
+));
